04bb68b503410560e0a5ef42053bb39303c5de41
Sayal Subba <sahilphago23@gmail.com>
2026-08-22 23:04:52 +0545

    adding useState

--- src/movie-search/movieSearch.jsx
+++ src/movie-search/movieSearch.jsx
@@ -1,17 +1,30 @@
 import React, { useState } from 'react'
 import Css from "./movieSearch.module.css"
 export default function movieSearch() {
-  const [search, setSearch] =useState("");
+  const [search, setSearch] = useState("");
+  const [movies, setMovies] = useState("");
+
+  const clickMovie = () => {
+    setSearch("");
+setMovies(search);
+  }
+
+
   return (
-<>
-<div className={Css["main"]}>
-<div className={Css["mainPrt"]}>
+    <>
+      <div className={Css["main"]}>
+        <div className={Css["mainPrt"]}>
+          <input type="text" placeholder='Enter Movie...' value={search} onChange={(e) => {
+            setSearch(e.target.value)
+          }} />
+          <button onClick={() => {
+            clickMovie()
+          }}>Search</button>
+          <h1>{search}</h1>
+          <h2>{movies}</h2>
+        </div>
 
-<input type="text" />
-<button>Search</button>
-</div>
-
-</div>
-</>
+      </div>
+    </>
   )
 }
